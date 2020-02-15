164edabf60a15cc8a069b642e6df4e10d51302f7
mozg3000 <i.luka@outlook.com>
2020-02-15 17:57:42 +0300

    expressiontree addNode method done without brackets "(", ")" parsing (processing) Please ente the commit essage for your changes. Lines starting

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,14 +1,32 @@
 import MNode from './js/mnode.js';
 import BinaryNode from './js/binarynode.js';
 import BinaryTree from './js/binarytree.js';
+import ExpressionTree from './js/expressiontree.js';
+import priorities from './js/priorities.js';
 
 
-let tree = new BinaryTree();
+// let tree = new BinaryTree();
 
-tree.addNode(50);
-tree.addNode(60);
-tree.addNode(40);
-tree.addNode(45);
-tree.addNode(55);
-tree.addNode(30);
-tree.printList();
+// tree.addNode(50)
+// tree.addNode(60);
+// tree.addNode(40);
+// tree.addNode(45);
+// tree.addNode(55);
+// tree.addNode(30);
+// tree.printList();
+// console.log(priorities)
+
+let tree = new ExpressionTree(priorities);
+
+// tree.addNode('1');
+// tree.addNode('+');
+tree.addNode('2');
+tree.addNode('+');
+tree.addNode('3');
+tree.addNode('*');
+tree.addNode('4');
+tree.addNode('+');
+tree.addNode('5');
+
+console.log(tree)
+// console.log(tree.getLast())
--- calculator/src/js/expressiontree.js
+++ calculator/src/js/expressiontree.js
@@ -1,10 +1,12 @@
 import BinaryTree from './binarytree.js';
-import BinaryNode from /./binarynode.js';
+import BinaryNode from './binarynode.js';
 
 export default class ExpressionTree extends BinaryTree{
-	constructor(expression, priorities){
+	constructor(priorities){
 		super();
 		this.priorities = priorities;
+		this.flag = false;
+		// this.pointer = this.head;
 		// this.currentHead
 		// this.expression = expression.split('');
 	}
@@ -36,32 +38,60 @@ export default class ExpressionTree extends BinaryTree{
 		if(!this.head){
 			this.addHead(symbol);
 		}else{
-			this.addElement();
+			this.addElement(symbol);
 		}
 	}
 	addHead(symbol){
 		this.head = new BinaryNode(symbol);
+		// this.pointer = this.head;
 	}
 	addElement(symbol){
 		let newPriority = this.getPriority(symbol);
-		let oldPriority = this.getPriority(this
-		if (newPriority < oldPriority) {
+		let oldPriority = this.getPriority(this.head.value);
+		let newNode= new BinaryNode(symbol);
+		// console.log(newNode);
+		// console.log(newPriority);
+		// console.log(newPriority);
+		if (newPriority <= oldPriority && newPriority !== 100) {
+			// console.log('smaller')
+			let pointer = this.getLast();
+			console.log(pointer)
+			if(pointer.next){
+				newNode.prev = pointer.next;
+				pointer.next = newNode;	
+			}else{
+				newNode.prev = this.head;
+				this.head = newNode;
+			}
 			
+		}else{
+			let pointer = this.getLast();
+			if(pointer.next){
+				pointer.next.next = newNode;
+			}else{
+				this.head.next = newNode;
+				// this.flag = true;
+			}
 		}
 	}
+	getLast(){
+		let pointer = this.head;
+		let prev = this.head;
+		while(pointer.next){
+			if(this.getPriority(pointer.next.value) === 100){
+				break;
+			}
+			prev = pointer;
+			pointer = pointer.next;
+		}
+		// console.log(prev)
+		return prev;
+	}
 	getPriority(symbol){
-		let priority;
-		if(!priority = this.priorities[symbol]){
+		let priority = this.priorities[symbol];
+		if(!priority){
 			priority = 100;
 		}
 		return priority;
 	}
 }
-// priorities = {
-	// '(': 90,
-	// ')': 90,
-	// '*': 80,
-	// '/': 70,
-	// '+': 50,
-	// '-': 50
-// };
